55aa41d2fe22125a39b3c26f2674adb60871f504
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2023-01-28 14:32:49 -0300

    Foramatação dados tabela nível conta

--- frontend_pdi/src/pages/contas/contas.vue
+++ frontend_pdi/src/pages/contas/contas.vue
@@ -4,13 +4,21 @@
       <h1><b>Contas</b></h1>
     </div>
     <b-container id="table-contas">
-        <b-table
-          bordered
-          striped
-          hover
-          :items="items"
-          :fields="fields"
-        ></b-table>
+      <b-table bordered striped hover :items="items" :fields="fields">
+        <template #cell(num_conta)="data">
+          {{ formatNumeroConta(data.item.num_conta) }}
+        </template>
+        <template #cell(situacao)="data">
+          {{ formatSituacao(data.item.situacao) }}
+        </template>
+        <template #cell(agencia_id_agencia)="data">
+          {{ formatAgencia(data.item.agencia_id_agencia) }}
+        </template>
+        <template #cell(tipo_conta)="data">
+          {{ formatTipoConta(data.item.tipo_conta) }}
+        </template>
+        </b-table
+      >
     </b-container>
   </div>
 </template>
@@ -52,6 +60,37 @@ export default {
     };
   },
   methods: {
+    formatNumeroConta(num_conta) {
+      let numeroTratado = num_conta.toString().slice(0,3) + '-' + num_conta.toString().slice(3, 6);
+      return numeroTratado;
+    },
+    formatAgencia(agencia){
+      let agenciaTratada = "";
+      if(agencia == 1){
+        agenciaTratada = "Belo Horizonte"
+      } else{
+        agenciaTratada = "São Paulo"
+      }
+      return agenciaTratada;
+    },
+    formatSituacao(situacao){
+      let situacaoTratada = "";
+      if(situacao == 1){
+        situacaoTratada = "Ativa"
+      } else {
+        situacaoTratada = "Inativa";
+      }
+      return situacaoTratada;
+    },
+    formatTipoConta(tipoConta){
+      let tipoContaTratado = "";
+      if(tipoConta == "F"){
+        tipoContaTratado = "Pessoa Física";
+      } else {
+        tipoContaTratado = "Pessoa Jurídica";
+      }
+        return tipoContaTratado;
+    },
     carregarContas() {
       ContasService.getContas()
         .then(response => {
