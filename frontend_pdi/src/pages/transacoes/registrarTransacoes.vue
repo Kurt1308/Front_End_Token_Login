cee4273203111502903cd16f6f299fcabd30f227
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2023-02-02 20:07:05 -0300

    Inclusão card para efetuar transação

--- frontend_pdi/src/pages/transacoes/registrarTransacoes.vue
+++ frontend_pdi/src/pages/transacoes/registrarTransacoes.vue
@@ -1,47 +1,63 @@
 <template>
-    <div style="width:100%; background-color: white;">
-      <div class="text-center" style="padding-top: 10%">
-        <h1><b>Registrar Transações</b></h1>
-      </div>
-      
+  <div style="width:100%; background-color: white;">
+    <div class="text-center" style="padding-top: 10%">
+      <h1><b>Registrar Transações</b></h1>
     </div>
-  </template>
-  
-  <script>
-  import { TransacoesService } from "../../services/transacoes/transacoesService.js";
-  export default {
-    data() {
-      return {
-      };
-    },
-    methods: {
-      carregarTransacoes() {
-        TransacoesService.getTransacoes()
-          .then(response => {
-            if (response.success) {
-              this.items = response.data.transacoes;
-            } else {
-              this.$toast.warning(
-                response.data.mensagem || response.statusMessage,
-                {
-                  position: "top-center",
-                  timeout: 5000,
-                  hideProgressBar: false
-                }
-              );
-            }
-          })
-          .catch(err => {
-            this.$toast.warning(err.message || err.statusMessage, {
-              position: "top-center",
-              timeout: 5000,
-              hideProgressBar: false
-            });
+    <div>
+      <b-button
+        style="margin-left:24%; margin-top:5%"
+        :class="visible ? null : 'collapsed'"
+        :aria-expanded="visible ? 'true' : 'false'"
+        aria-controls="collapse-4"
+        @click="visible = !visible"
+      >
+        Efetuar Transação
+      </b-button>
+      <div style="margin-left:24%; margin-top:2%">
+        <container>
+          <b-collapse id="collapse-4" v-model="visible" class="mt-2">
+            <b-card style="width: 48rem;">I should start open!</b-card>
+          </b-collapse>
+        </container>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { TransacoesService } from "../../services/transacoes/transacoesService.js";
+export default {
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
+    carregarTransacoes() {
+      TransacoesService.getTransacoes()
+        .then(response => {
+          if (response.success) {
+            this.items = response.data.transacoes;
+          } else {
+            this.$toast.warning(
+              response.data.mensagem || response.statusMessage,
+              {
+                position: "top-center",
+                timeout: 5000,
+                hideProgressBar: false
+              }
+            );
+          }
+        })
+        .catch(err => {
+          this.$toast.warning(err.message || err.statusMessage, {
+            position: "top-center",
+            timeout: 5000,
+            hideProgressBar: false
           });
-      }
-    },
-    created() {
+        });
     }
-  };
-  </script>
-  
+  },
+  created() {}
+};
+</script>
