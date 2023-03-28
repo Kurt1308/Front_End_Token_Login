1c17192f8c087db4e1d3083c7bbce08a273c5a97
Lucas Dias <98557406+Kurt1308@users.noreply.github.com>
2023-03-28 12:25:44 -0300

    Inclusão acesso para detalhes do cartão

--- frontend_pdi/src/pages/cartoes/cartoes.vue
+++ frontend_pdi/src/pages/cartoes/cartoes.vue
@@ -19,7 +19,26 @@
         </template>
 
         <template #row-details="row">
-          <b-card>
+          <div v-if="acessoLiberado == false">
+            <b-row>
+              <b-col sm="2" class="text-sm-right"
+                ><label
+                  ><b><h4>Digite sua senha:</h4></b></label
+                ></b-col
+              >
+              <b-col sm="2" class="text-sm-right"
+                ><b-form-input v-model="senhaInterna"></b-form-input></b-col
+              ><b-col sm="0.5" class="text-sm-right"
+                ><b-button
+                  style="margin-top:21%"
+                  size="sm"
+                  @click="pedirAcesso()"
+                  >Acessar</b-button
+                ></b-col
+              >
+            </b-row>
+          </div>
+          <b-card v-if="acessoLiberado == true">
             <b-row class="mb-2">
               <b-col sm="2" class="text-sm-right"><b>Ano/Mês Venc:</b></b-col>
               <b-col>{{ formatAnoMesVenc(row.item.mes_ano_vencimento) }}</b-col>
@@ -29,13 +48,15 @@
             </b-row>
 
             <b-row class="mb-2">
-              <b-col sm="2" class="text-sm-right"><b>Limite Disponível:</b></b-col>
+              <b-col sm="2" class="text-sm-right"
+                ><b>Limite Disponível:</b></b-col
+              >
               <b-col>{{ formatDecimal(row.item.limite_saldo) }}</b-col>
 
-              <b-col sm="2" class="text-sm-right"
-                ><b>Limite Saldo:</b></b-col
-              >
-              <b-col>{{ formatDecimal(row.item.limite_saldo_disponivel) }}</b-col>
+              <b-col sm="2" class="text-sm-right"><b>Limite Saldo:</b></b-col>
+              <b-col>{{
+                formatDecimal(row.item.limite_saldo_disponivel)
+              }}</b-col>
             </b-row>
 
             <b-button size="sm" @click="row.toggleDetails"
@@ -53,30 +74,58 @@ import { CartoesService } from "../../services/cartoes/cartoesService.js";
 export default {
   data() {
     return {
+      acessoLiberado: false,
       fields: [
         //{ key: "conta_id_conta", label: "Id Conta" },
         { key: "num_cartao", label: "Número do Cartão" },
         { key: "show_details", label: "Detalhes" }
       ],
-      items: [
-      ]
+      items: []
     };
   },
   methods: {
-    formatNumeroCartao(numCartao){
-      let numeroTratado = numCartao.toString().slice(0,4) + '-' + numCartao.toString().toString().slice(4, 8) + '-' + numCartao.toString().toString().slice(8, 12) + '-' + numCartao.toString().toString().slice(12, 16);
+    formatNumeroCartao(numCartao) {
+      let numeroTratado =
+        numCartao.toString().slice(0, 4) +
+        "-" +
+        numCartao
+          .toString()
+          .toString()
+          .slice(4, 8) +
+        "-" +
+        numCartao
+          .toString()
+          .toString()
+          .slice(8, 12) +
+        "-" +
+        numCartao
+          .toString()
+          .toString()
+          .slice(12, 16);
       return numeroTratado;
     },
-    formatAnoMesVenc(data){
+    formatAnoMesVenc(data) {
       let dataTratada = "";
-      if(data.length < 7){
-        dataTratada = data.toString().slice(0,4) + '-' + data.toString().toString().slice(4, 6);
+      if (data.length < 7) {
+        dataTratada =
+          data.toString().slice(0, 4) +
+          "-" +
+          data
+            .toString()
+            .toString()
+            .slice(4, 6);
       } else {
-        dataTratada = data.toString().slice(6,10) + '-' + data.toString().toString().slice(3, 5);
+        dataTratada =
+          data.toString().slice(6, 10) +
+          "-" +
+          data
+            .toString()
+            .toString()
+            .slice(3, 5);
       }
       return dataTratada;
     },
-    formatDecimal(valor){
+    formatDecimal(valor) {
       let valorTratado = valor.toFixed(2);
       return valorTratado;
     },
